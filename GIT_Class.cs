5f006fdccacfee918fb06f4bd87d2d8b605d5c4a
Marcelin Irudayaraj <marcelin.ss@outlook.com>
2020-04-27 19:09:41 +0530

    Revert "Revert test"
    This reverts commit ac32fa10fff43d6dac4da65f07f04e13f106ec50.

--- GIT_Class.cs
+++ GIT_Class.cs
@@ -7,5 +7,12 @@ namespace GIT_Test
 {
     public class GIT_Class
     {
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Just Updated code");
+            Console.WriteLine("Just Updated code to Develop");
+            Console.ReadLine();
+        }
     }
 }
